e4bf25c6da6a93e91eec049ad7f45b52a3ddedac
Aswin Infant Joseph <ashinfant@gmail.com>
2025-01-07 15:56:46 +0530

    Add files via upload

---
+++ alansbuild/script.js
@@ -0,0 +1,10 @@
+// script.js
+window.addEventListener('scroll', () => {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        navbar.style.backgroundColor = 'rgba(0, 0, 0, 0.8)';
+    } else {
+        navbar.style.backgroundColor = 'transparent';
+    }
+});
+
